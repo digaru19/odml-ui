1c296ab739f1e5da935c5fe9c2339fdcede8f085
digaru19 <digsblogger@gmail.com>
2017-07-07 15:45:16 +0530

    popopop

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# This script executes outside of the virtual environment,
+# hence the packages installed will be system-wide, and not 
+# specifically in the virtual environment.
+
 python --version
 python3 --version
 
 sudo apt-get -qq update
 sudo apt-get install libffi-dev libglib2.0-0 libglib2.0-dev
 sudo apt-get install gobject-introspection
-sudo apt-get install python-gi python3-gi # python-gi-dev # python3-gi-cairo 
+sudo apt-get install python-gi python3-gi
 sudo apt-get install gir1.2-glib-2.0 
 
 python -c "import gi"
