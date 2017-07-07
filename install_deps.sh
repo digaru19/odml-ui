030430f8a1a0b635d0635cda7fff1a081e7a6a2c
digaru19 <digsblogger@gmail.com>
2017-07-07 12:38:34 +0530

    aspaos

--- install_deps.sh
+++ install_deps.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+python3 --version
 sudo apt-get update
-sudo apt-get upgrade
+# sudo apt-get upgrade
 sudo apt-get install libffi-dev libglib2.0-0 libglib2.0-dev
 sudo apt-get install python python3
-sudo apt-get install python3-dev python3-pip
+sudo apt-get install python-dev python-pip python3-dev python3-pip
 sudo apt-get install gobject-introspection
-sudo apt-get install python3-gi python3-gi-cairo python-gi-dev
-sudo apt-get install gir1.2-gtk-3.0
+sudo apt-get install python-gi python3-gi python3-gi-cairo python-gi-dev
+sudo apt-get install gir1.2-gtk-3.0 gir1.2-ibus-1.0
 
 echo -e "\n\n #########################################"
 echo -e "\n\t Installed all deps !!!"
