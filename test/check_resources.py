6ac60c129fa4798352473225c0565a8fdef730d3
digaru19 <digsblogger@gmail.com>
2017-07-07 14:07:23 +0530

    asaosk

--- test/check_resources.py
+++ test/check_resources.py
@@ -19,11 +19,16 @@ if not os.path.isfile(applications + 'odml.desktop'):
     print("Not found :- %s" % (applications + 'odml.desktop'))
     exit(1)
 
-try:
-    import gi
-    from gi.repository import Gtk
-except Exception as e:
-    print(e)
-    exit(1)
+### If importing 'gi' works in a virtual environment, we can
+### use the following method to check if 'Gtk' dependencies
+### are installed properly. Till then, this check happens in 
+### the main travis build file.
+
+# try:
+#     import gi
+#     from gi.repository import Gtk
+# except Exception as e:
+#     print(e)
+#     exit(1)
 
 exit(0)
