0dc3985ca397ca9e4a7e67c2c543a617850df9d6
digaru19 <digsblogger@gmail.com>
2017-07-07 14:31:09 +0530

    aoisdo

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,8 +6,8 @@ python3 --version
 sudo apt-get -qq update
 sudo apt-get install libffi-dev libglib2.0-0 libglib2.0-dev
 sudo apt-get install gobject-introspection
-sudo apt-get install python-gi python3-gi python3-gi-cairo python-gi-dev
-sudo apt-get install gir1.2-glib-2.0 # libgtk-3-dev
+sudo apt-get install python-gi python3-gi # python-gi-dev # python3-gi-cairo 
+sudo apt-get install gir1.2-glib-2.0 
 
 python -c "import gi"
 python -c "from gi import pygtkcompat"
