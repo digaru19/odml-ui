02fe97c4baf7c6a7f187d59a8ba0dd2b0f1631c3
digaru19 <digsblogger@gmail.com>
2017-07-08 22:24:07 +0530

    OS X first

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,28 +5,40 @@
 # specifically in the virtual environment.
 
 set -ev
-
 echo $TRAVIS_PYTHON_VERSION
 
-if [ ${TRAVIS_PYTHON_VERSION%.*} -eq 3 ]
+if [[ $TRAVIS_OS_NAME == 'linux' ]]
 then
-    packages='python3-gi'
+    
+    if [ ${TRAVIS_PYTHON_VERSION%.*} -eq 3 ]
+    then
+        packages='python3-gi'
+    else
+        packages='python-gi'
+    fi
+    sudo apt-get -qq update
+    sudo apt-get install libffi-dev libglib2.0-0 libglib2.0-dev
+    sudo apt-get install gobject-introspection libgtk-3-dev
+    sudo apt-get install ${packages}
+
+    if [ ${TRAVIS_PYTHON_VERSION%.*} -eq 3 ]
+    then
+        python3 test/gi_imports.py
+    else
+        python test/gi_imports.py
+    fi
+    
 else
-    packages='python-gi'
+    brew install libffi
+    brew install glib
+    brew install gobject-introspection --with-python3
+    brew install pygobject3 --with-python3
+
+
+
 fi
 
-sudo apt-get -qq update
-sudo apt-get install libffi-dev libglib2.0-0 libglib2.0-dev
-sudo apt-get install gobject-introspection
-sudo apt-get install ${packages}
-sudo apt-get install libgtk-3-dev # gir1.2-glib-2.0 
 
-if [ ${TRAVIS_PYTHON_VERSION%.*} -eq 3 ]
-then
-    python3 test/gi_imports.py
-else
-    python test/gi_imports.py
-fi
 
 echo -e "\n\n #########################################"
 echo -e " Installed all dependencies successfully !!"
