460d42c33cb36fb36cdd4d7aad072b6c77d514c2
digaru19 <digsblogger@gmail.com>
2017-07-08 15:21:30 +0530

    Squash 3

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,19 +6,28 @@
 
 set -ev
 
-python --version
-python3 --version
-
 echo $TRAVIS_PYTHON_VERSION
+
+if [ ${TRAVIS_PYTHON_VERSION%.*} -eq 3 ]
+then
+    packages='python3-gi'
+else
+    packages='python-gi'
+fi
 
 sudo apt-get -qq update
 sudo apt-get install libffi-dev libglib2.0-0 libglib2.0-dev
 sudo apt-get install gobject-introspection
-sudo apt-get install python-gi python3-gi 
+# sudo apt-get install python-gi python3-gi 
+sudo apt-get install ${packages}
 sudo apt-get install libgtk-3-dev gir1.2-glib-2.0 
 
-python test/gi_imports.py
-python3 test/gi_imports.py
+if [ ${TRAVIS_PYTHON_VERSION%.*} -eq 3 ]
+then
+    python3 test/gi_imports.py
+else
+    python test/gi_imports.py
+fi
 
 echo -e "\n\n #########################################"
 echo -e " Installed all dependencies successfully !!"
